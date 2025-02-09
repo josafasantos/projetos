fcbf39ee025b861444a871adf2aad8069d5f1d8e
Josafa Santos <josafaalvesdossantos@gmail.com>
2025-02-09 10:11:22 -0300

    Adicionando projeto Hello World Interativo

---
+++ Javascript/01-hello-world-interativo/script.js
@@ -0,0 +1,7 @@
+// Exibe "Hello World" no console
+console.log("Hello World!");
+
+// Função para exibir um alerta quando o botão for clicado
+function exibirMensagem() {
+  alert("Olá, mundo! Este é seu primero projeto em Javascript");
+}
