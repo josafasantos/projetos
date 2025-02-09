77fa7059d67bcbfe04bfaea307f85a44e6c2c7c8
Josafa Santos <josafaalvesdossantos@gmail.com>
2025-02-08 21:05:03 -0300

    Adiciona tabuada automática

---
+++ Python/08-tabuada-automatica/08-tabuada-automatica.py
@@ -0,0 +1,9 @@
+# Tabuada Automática
+
+# Solicita um número ao usuário
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+# Exibe a tabuada de 1 a 10
+print(f"\nTabuada do {numero}")
+for i in range(1,11):
+  print(f"{numero} x {i} = {numero * i}")
